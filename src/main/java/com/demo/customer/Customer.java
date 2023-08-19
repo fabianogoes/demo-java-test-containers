d9052338a834299d115c7b4408a46220957c640b
Fabiano Góes <fabianogoes@gmail.com>
2023-08-18 23:52:08 -0300

    Add Insomnia Collection

--- src/main/java/com/demo/customer/Customer.java
+++ src/main/java/com/demo/customer/Customer.java
@@ -2,7 +2,6 @@ package com.demo.customer;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
